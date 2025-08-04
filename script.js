92c0d84373a3e72a4444b55be662f82bb04861b6
TobiMessi <kajzertobiasz@gmail.com>
2025-08-04 15:54:02 +0200

    Update script.js
    Signed-off-by: TobiMessi <kajzertobiasz@gmail.com>

--- script.js
+++ script.js
@@ -113,3 +113,23 @@ function downloadPDF() {
     };
     html2pdf().set(opt).from(element).save();
 }
+const toggle = document.getElementById('toggle-darkmode');
+
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'disabled');
+  }
+});
+
+// Przy załadowaniu strony sprawdź preferencje
+window.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    toggle.checked = true;
+  }
+});
+
